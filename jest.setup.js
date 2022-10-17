1f3f882caa9cbfc2cada7cdea531efc0bda630d4
reactdb1984@gmail.com <reactdb1984@gmail.com>
2022-10-17 13:52:52 +0100

    Initial commit from Create Next App

---
+++ jest.setup.js
@@ -0,0 +1,21 @@
+import '@testing-library/jest-dom/extend-expect'
+
+global.IntersectionObserver = class IntersectionObserver {
+    constructor() { }
+
+    disconnect() {
+        return null;
+    }
+
+    observe() {
+        return null;
+    }
+
+    takeRecords() {
+        return null;
+    }
+
+    unobserve() {
+        return null;
+    }
+};
